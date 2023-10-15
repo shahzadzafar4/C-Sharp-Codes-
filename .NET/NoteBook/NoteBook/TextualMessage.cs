fc34c7affb70595ed4f33165e709b78ed3f1724e
Shahzad zafar   .NET Developer | C# Enthusiast | Problem Solver <119974504+shahzadzafar4@users.noreply.github.com>
2023-10-16 00:37:11 +0500

    Create TextualMessage.cs

---
+++ .NET/NoteBook/NoteBook/TextualMessage.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteBook
+{
+    class TextualMessage : IPageable
+    {
+        protected PageData myData;
+        protected string message;
+
+        public virtual IPageable Input()
+        {
+            Console.WriteLine("Please Input your name: ");
+            myData.author = Console.ReadLine();
+            Console.WriteLine("Please Input the Message Title: ");
+            myData.title = Console.ReadLine();
+            Console.WriteLine("Please Input the Message: ");
+            message = Console.ReadLine();
+            return this;
+        }
+
+        public void Output()
+        {
+            Console.WriteLine();
+            Console.WriteLine("//----------------------------Message----------------------------//");
+            Console.WriteLine("Title: " + myData.title);
+            Console.WriteLine("Auther: " + myData.author);
+            Console.WriteLine("Message: \n \n" + message);
+            Console.WriteLine("//---------------------------------------------------------------//");
+            
+        }
+
+        public PageData MyData 
+        {
+            get
+            {
+                return MyData;
+            }
+            set
+            {
+                MyData = value;
+            }
+        }
+    }
+}
