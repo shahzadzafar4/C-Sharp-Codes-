b4e5212f6a92a06b6855f411f6894ddfaf2ab348
Shahzad zafar   .NET Developer | C# Enthusiast | Problem Solver <119974504+shahzadzafar4@users.noreply.github.com>
2023-10-16 00:37:46 +0500

    Create Program.cs

---
+++ .NET/Overloading/Program.cs
@@ -0,0 +1,25 @@
+﻿using Overloading;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading_Operators
+{
+    class Program
+    {
+        void Main(string[] args)
+        {
+            Message message = new Message();
+            Message message2 = new Message();
+            message.BodyOfText = "Hello";
+            message2.BodyOfText = "Hey";
+
+            message = message + message2;
+
+            Console.WriteLine(message.BodyOfText);
+            Console.ReadLine();
+        }
+    }
+}
