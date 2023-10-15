c36efbfb32c4ea70389f5adc0bfdf48367bd05e7
Shahzad zafar   .NET Developer | C# Enthusiast | Problem Solver <119974504+shahzadzafar4@users.noreply.github.com>
2023-10-16 00:36:39 +0500

    Create NoteBookLogger.cs

---
+++ .NET/NoteBook/NoteBook/NoteBookLogger.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteBook
+{
+    class NoteBookLogger
+    {
+        private NoteBook trackkedNoteBook;
+        private bool Logging = true;
+
+        NoteBookLogger(NoteBook trackkedNoteBook)
+        {
+            this.trackkedNoteBook = trackkedNoteBook;
+        }
+
+        private void PrintAdded(string typeItemAdded)
+        {
+
+        }
+
+        private void PrintDeleted(string idOfDeleted)
+        {
+
+        }
+        private void IncorrectCommand(string MessageToPrint)
+        {
+
+        }
+
+        public void ToggleLogging(bool isLoggingon)
+        {
+
+        }
+
+        private void Attach()
+        {
+
+        }
+
+        private void Detach()
+        {
+
+        }
+    }
+}
