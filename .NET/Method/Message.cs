c5a497c69c5cabfef1d94137b119327108ec35d2
Shahzad zafar   .NET Developer | C# Enthusiast | Problem Solver <119974504+shahzadzafar4@users.noreply.github.com>
2023-10-16 00:35:32 +0500

    Create Message.cs

---
+++ .NET/Method/Message.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    public class Message
+    {
+        string BodyOfTexy = " Shahzad Zafar", Text = "You will achieve everything soon....";
+        const string cpmpanyName = "ABC Industries";
+        readonly string CompanyVision = "World's no 1 company";
+
+        public void SetMessage (ref string message)
+        {
+            message += BodyOfTexy;
+        }
+        public void text (out string message1)
+            {
+            message1 = Text;
+
+        }
+
+
+        // overloaded method
+        //  public void SetMessage(string message, string whattoset)
+        //  {
+        //      message = whattoset;
+        //   }
+    }
+}
