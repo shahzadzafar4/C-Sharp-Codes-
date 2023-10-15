199cedbc80569f9ae65f906bd6626d150031d087
Shahzad zafar   .NET Developer | C# Enthusiast | Problem Solver <119974504+shahzadzafar4@users.noreply.github.com>
2023-10-16 00:36:27 +0500

    Create IPageable.cs

---
+++ .NET/NoteBook/NoteBook/IPageable.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteBook
+{
+    public struct PageData
+    {
+        public string title, author;
+    }
+
+    public interface IPageable
+    {
+        PageData MyData { get; set; }
+
+        // How we are going to input our item
+        IPageable Input();
+
+        // How do we do out put 
+        void Output();
+
+    }
+}
