bef1ce206c57c94d5b46ca3fb2c97c869e0055d0
Shahzad zafar   .NET Developer | C# Enthusiast | Problem Solver <119974504+shahzadzafar4@users.noreply.github.com>
2023-10-16 00:35:58 +0500

    Create Program.cs

---
+++ .NET/Method/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            // method with ref key word
+            Message message = new Message();
+            string PieceOfText = "Hello:";
+            message.SetMessage(ref PieceOfText);
+            Console.WriteLine(PieceOfText);
+            Console.ReadLine();
+
+            // method with out key word 
+            Message message1 = new Message();
+            string text;
+            message1.text(out text);
+            Console.WriteLine(text);
+            Console.ReadLine();
+
+            
+           
+            // overloaded method
+           // message.SetMessage(PieceOfText, "What's up");
+        }
+    }
+}
